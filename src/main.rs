bfaeb432d4d579d1d905dc459ae06e1460686d41
Matt Gleich <git@mattglei.ch>
2022-04-07 23:50:38 -0400

    feat: fetch activites
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ use dotenv::dotenv;
 use reqwest::blocking::Client;
 use strava::AuthData;
 
+use crate::strava::Activity;
+
 mod strava;
 
 fn main() {
     dotenv().ok();
     let client = Client::new();
-    let auth_data = AuthData::new(&client).expect("Failed to load authdata");
-    dbg!(auth_data);
+    let mut auth_data = AuthData::new(&client).expect("Failed to load authdata");
+    dbg!(&auth_data);
+    auth_data
+        .request_new_token(&client)
+        .expect("Failed to request new token");
+    dbg!(&auth_data);
+
+    let activites =
+        Activity::fetch_recent(&auth_data, &client).expect("Failed to fetch recent activates");
+    dbg!(activites);
 }
--- src/strava.rs
+++ src/strava.rs
@@ -1,16 +1,33 @@
 use std::env;
 
 use anyhow::{ensure, Result};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use reqwest::{blocking::Client, StatusCode, Url};
+use serde::Deserialize;
 
 const STRAVA_URL: &str = "https://www.strava.com";
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct AuthData {
     pub client_secret: String,
     pub client_id: String,
     pub refresh_token: String,
     pub access_token: String,
+    pub token_expire_time: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Activity {
+    pub elapsed_time: f32,
+    #[serde(rename(deserialize = "type"))]
+    pub activity_type: String,
+    pub average_heartrate: f32,
+    pub max_heartrate: f32,
+    pub average_speed: f32,
+    pub max_speed: f32,
+    pub moving_time: f32,
+    pub name: String,
+    pub pr_count: f32,
 }
 
 impl AuthData {
@@ -32,13 +49,53 @@ impl AuthData {
             resp.status() == StatusCode::OK,
             "Response didn't have a status code of 200"
         );
-        let data = resp.json::<response::InitialCode>()?;
+        let data = resp.json::<response::Token>()?;
         Ok(AuthData {
             client_secret,
             client_id,
             refresh_token: data.refresh_token,
             access_token: data.access_token,
+            token_expire_time: DateTime::from_utc(
+                NaiveDateTime::from_timestamp(data.expires_at.into(), 0),
+                Utc,
+            ),
         })
+    }
+
+    pub fn request_new_token(&mut self, client: &Client) -> Result<()> {
+        let url = Url::parse_with_params(
+            &format!("{}/api/v3/oauth/token", STRAVA_URL),
+            &[
+                ("client_id", &self.client_id),
+                ("client_secret", &self.client_secret),
+                ("grant_type", &String::from("refresh_token")),
+                ("refresh_token", &self.refresh_token),
+            ],
+        )?;
+        let resp = client.post(url).send()?;
+        let data = resp.json::<response::Token>()?;
+        self.refresh_token = data.refresh_token;
+        self.token_expire_time = DateTime::from_utc(
+            NaiveDateTime::from_timestamp(data.expires_at.into(), 0),
+            Utc,
+        );
+        self.access_token = data.access_token;
+        Ok(())
+    }
+}
+
+impl Activity {
+    pub fn fetch_recent(auth_data: &AuthData, client: &Client) -> Result<Vec<Self>> {
+        let resp = client
+            .get(format!("{}/api/v3/athlete/activities", STRAVA_URL))
+            .bearer_auth(&auth_data.access_token)
+            .send()?;
+        ensure!(
+            resp.status() == StatusCode::OK,
+            "Response didn't have status code of 200"
+        );
+        let data = resp.json::<Vec<Activity>>()?;
+        Ok(data)
     }
 }
 
@@ -46,8 +103,9 @@ mod response {
     use serde::Deserialize;
 
     #[derive(Deserialize)]
-    pub struct InitialCode {
+    pub struct Token {
         pub refresh_token: String,
         pub access_token: String,
+        pub expires_at: u32,
     }
 }
